bbe4513c1685c9880e369958f2ced4963dbd8cc6
yupitseli <eliascrosby10@gmail.com>
2026-09-23 11:34:28 -0700

    Create NoSunshine.java

---
+++ NoSunshine.java
@@ -0,0 +1,14 @@
+public class NoSunshine {
+   public static void main(String[] args) {
+   aintNo("Ain't no sunshine when she's gone");
+   aintNo("It's not warm when she's away");
+   aintNo("Ain't no sunshine when she's gone");
+   aintNo("And she's always gone too long");
+   aintNo("Anytime she goes away");
+   }
+   public static void aintNo(String songLine) {
+      System.out.println( songLine );
+   }
+}
+   
+   
