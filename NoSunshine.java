3c5a8e90abafbb43d1d675030f1eaec82e890990
yupitseli <eliascrosby10@gmail.com>
2026-09-24 12:52:44 -0700

    Update NoSunshine.java

--- NoSunshine.java
+++ NoSunshine.java
@@ -1,22 +1,3 @@
-public class NoSunshine {
-   public static void main(String[] args) {
-   n();a("It's not warm when she's away");n();a("And she's always gone too long");g();a("Wonder this time where she's gone");a("Wonder if she's gone to stay");n();h();g();a("And I know, I know, I know, I know");i();i();i();i();a("I know, I know");a("Hey, I ought to leave young thing alone");a("But aint no sunshine when she's gone, whoa-whoa");n();a("Only darkness every day");n();h();g();g();g();g();
-   }
-   public static void a(String songLine) {
-      System.out.println(songLine);
-   }
-     public static void i() {
-      System.out.println("I know, I know, I know, I know, I know");
-   }
-   public static void g() {
-   System.out.println("Anytime she goes away");
-   }
-   public static void n() {
-   System.out.println("Ain't no sunshine when she's gone");
-   }
-   public static void h() {
-   System.out.println("And this house just ain't no home");
-   }
-}
+public class NoSunshine {public static void main(String[] args) {n();a("It's not warm when she's away");n();a("And she's always gone too long");g();a("Wonder this time where she's gone");a("Wonder if she's gone to stay");n();h();g();a("And I know, I know, I know, I know");i();i();i();i();a("I know, I know");a("Hey, I ought to leave young thing alone");a("But aint no sunshine when she's gone, whoa-whoa");n();a("Only darkness every day");n();h();g();g();g();g();}public static void a(String songLine){System.out.println(songLine);}public static void i(){System.out.println("I know, I know, I know, I know, I know");}public static void g(){System.out.println("Anytime she goes away");}public static void n(){System.out.println("Ain't no sunshine when she's gone");}public static void h(){System.out.println("And this house just ain't no home");}}
    
    
