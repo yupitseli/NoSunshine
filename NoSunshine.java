495277b83f8545f6bc0c264dc560d92043366243
yupitseli <eliascrosby10@gmail.com>
2026-09-24 12:29:33 -0700

    Update NoSunshine.java

--- NoSunshine.java
+++ NoSunshine.java
@@ -1,13 +1,43 @@
 public class NoSunshine {
    public static void main(String[] args) {
-   aintNo("Ain't no sunshine when she's gone");
-   aintNo("It's not warm when she's away");
-   aintNo("Ain't no sunshine when she's gone");
-   aintNo("And she's always gone too long");
-   aintNo("Anytime she goes away");
+   n();
+   a("It's not warm when she's away");
+   n();
+   a("And she's always gone too long");
+   g();
+   a("Wonder this time where she's gone");
+   a("Wonder if she's gone to stay");
+   n();
+   a("And this house just ain't no home");
+   g();
+   a("And I know, I know, I know, I know");
+   i();
+   i();
+   i();
+   i();
+   a("I know, I know");
+   a("Hey, I ought to leave young thing alone");
+   a("But ain't no sunshine when she's gone, whoa-whoa");
+   n();
+   a("Only darkness every day");
+   n();
+   a("And this house just ain't no home");
+   g();
+   g();
+   g();
+   g();
    }
-   public static void aintNo(String songLine) {
-      System.out.println( songLine );
+   public static void a(String songLine) {
+      System.out.println(songLine);
+   }
+     public static void i() {
+      System.out.println("I know, I know, I know, I know, I know");
+   }
+   public static void g() {
+   System.out.println("Anytime she goes away");
+   }
+   public static void n() {
+   System.out.println("Ain't no sunshine when she's gone");
    }
 }
    
